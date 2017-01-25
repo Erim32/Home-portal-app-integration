d62da935140e2215ec907aa4ce230151bf5233be
Rémi M <contact@erim.fr>
2017-01-25 12:59:22 +0100

    add map location to variables

--- js/variables.js
+++ js/variables.js
@@ -1,15 +1,15 @@
 var apikey = {
-		weather_com		: "XXXXXXXXXXXXXXXXXXXXXXX",
-		google_map		: "XXXXXXXXXXXXXXXXXXXXXXX",
+		weather_com	: "XXXXXXXXXXXXXXXXXXXXXXX",
+		google_map	: "XXXXXXXXXXXXXXXXXXXXXXX",
 		google_calendar : "XXXXXXXXXXXXXXXXXXXXXXX"
 };
 
 var calendrier = {
-	calendarId	: "XXXXXXXXXXXXXXXXXXXXXXX"
+	calendarId  : "XXXXXXXXXXXXXXXXXXXXXXX"
 };
 var todolist = {
-	 colonne_home 			: 1, //[0 - 2]
-	 url_fichier_php 		: "./to-do-list/write.php",
+	 colonne_home    	: 1, //[0 - 2]
+	 url_fichier_php        : "./to-do-list/write.php",
 	 url_fichier_sauvegarde : "./to-do-list/liste.txt", // A modifier dans le fichier php egalement
 	 url_fichier_original   : "./to-do-list/liste.txt.original" // A modifier dans le fichier php egalement
 };
@@ -18,6 +18,11 @@ var meteo = {
 	location	: "Toulouse",
 	pays 		: "France",
 	codePays	: "FR"
+};
+
+var carte = {
+		lat  : 43.600000,
+		long : 1.433333
 };
 
 var rss = {
